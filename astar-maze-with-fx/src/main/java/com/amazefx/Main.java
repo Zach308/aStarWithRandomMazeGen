64adb12721bbfb99f52af921b00b5bbfb8532c16
null <92740163+Zach308@users.noreply.github.com>
2022-12-02 11:43:23 -0600

    Add files via upload

---
+++ astar-maze-with-fx/src/main/java/com/amazefx/Main.java
@@ -0,0 +1,17 @@
+package com.amazefx;
+
+
+import com.amazefx.GUI.MainGUI;
+
+/**
+ * JavaFX App
+ */
+public class Main {
+    public static void main(String[] args) {
+        /**
+         * call static gui method
+         */
+        MainGUI.showGUI();
+    }
+
+}
---
+++ astar-maze-with-fx/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module com.amazefx {
+    requires transitive javafx.controls;
+    requires transitive javafx.fxml;
+
+    opens com.amazefx.GUI to javafx.fxml;
+    exports com.amazefx.GUI;
+}
